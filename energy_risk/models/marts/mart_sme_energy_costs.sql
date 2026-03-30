5ab369a461989b2ce4191f14da63300f43fb62f1
Michael Garcia <michael.garcia.73@gmail.com>
2026-03-30 23:41:17 +0200

    dbt: partition mart by month, cluster by price_zone and SME segment

--- energy_risk/models/marts/mart_sme_energy_costs.sql
+++ energy_risk/models/marts/mart_sme_energy_costs.sql
@@ -1,31 +1,54 @@
+{{
+  config(
+    materialized = "table",
+    partition_by = {
+      "field": "ts_utc",
+      "data_type": "timestamp",
+      "granularity": "month"
+    },
+    cluster_by = ["price_zone", "segment"]
+  )
+}}
+
 with prices as (
     select
         ts_utc,
-        country_code,
+        price_zone,
         price_eur_mwh,
-        -- prix en EUR/kWh
+        -- price in EUR/kWh
         price_eur_mwh / 1000.0 as price_eur_kwh
     from {{ ref('stg_opsd_prices') }}
 ),
 
 sme as (
-    select * from {{ ref('sme_energy_profile') }}
+    select
+        segment,
+        segment_size,
+        industry,
+        avg_kwh_per_day,
+        energy_cost_share,
+        base_margin_pct
+    from {{ ref('sme_energy_profile') }}
 ),
 
 daily_costs as (
     select
-        date(ts_utc) as date,
-        p.country_code,
+        p.ts_utc,
+        date(p.ts_utc) as date,
+        p.price_zone,
         s.segment,
+        s.segment_size,
+        s.industry,
         s.avg_kwh_per_day,
         s.base_margin_pct,
         s.energy_cost_share,
-        -- coût énergie quotidien
+        -- daily energy cost
         s.avg_kwh_per_day * p.price_eur_kwh as energy_cost_eur,
-        -- revenu approximatif (en supposant que l'énergie représente energy_cost_share des coûts)
+        -- approximate total cost (assuming energy_cost_share is the share of total costs)
         (s.avg_kwh_per_day * p.price_eur_kwh) / s.energy_cost_share as total_cost_eur,
+        -- approximate revenue given base margin
         (( (s.avg_kwh_per_day * p.price_eur_kwh) / s.energy_cost_share ) / (1 - s.base_margin_pct)) as revenue_eur,
-        -- profit brut simplifié
+        -- simplified gross profit
         (
           (( (s.avg_kwh_per_day * p.price_eur_kwh) / s.energy_cost_share ) / (1 - s.base_margin_pct))
           - (s.avg_kwh_per_day * p.price_eur_kwh) / s.energy_cost_share
